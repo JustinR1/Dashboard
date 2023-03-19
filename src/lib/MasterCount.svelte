05f3c60df59e37a69ea38e231057b1b0ab6e3fda
Justin <zeroproof@protonmail.com>
2023-03-19 11:02:58 -0400

    Update MasterCount.svelte

--- src/lib/MasterCount.svelte
+++ src/lib/MasterCount.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import { phantasmaJS } from "phantasma-ts";
+//     import { phantasmaJS } from "phantasma-ts";
 
     let soulMaster = "";
 
