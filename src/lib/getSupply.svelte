67c4d597f6c25b9eaef1c2335f97786cbbf731ee
Justin <zeroproof@protonmail.com>
2023-03-19 12:15:55 -0400

    Add files via upload

---
+++ src/lib/getSupply.svelte
@@ -0,0 +1,69 @@
+<script>
+    import { onMount } from "svelte";
+
+    let soulToken = "";
+    let kcalToken = "";
+    let soulSupply = null;
+    let kcalSupply = null;
+
+    function formatNumberWithCommas(number) {
+        const [integerPart, decimalPart] = number.toString().split(".");
+        return integerPart.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+    
+
+    async function getSupply() {
+        try {
+            const response = await fetch(
+                "http://pharpc1.phantasma.io:7077/api/v1/GetNexus?extended=true"
+            );
+            const data = await response.json();
+            let soul = data.tokens.find((token) => token.symbol === "SOUL");
+            let kcal = data.tokens.find((token) => token.symbol === "KCAL");
+
+            if (soul) {
+                soulToken = soul.symbol;
+                console.log(soulToken);
+            }
+
+            if (kcal) {
+                kcalToken = kcal.symbol;
+                console.log(kcalToken);
+            }
+
+            soulSupply = soul.currentSupply;
+            let soulSupplyDecimal = Math.pow(10, 8);
+            soulSupply = soulSupply / soulSupplyDecimal;
+            soulSupply = formatNumberWithCommas(soulSupply);
+            console.log(soulSupply);
+
+            kcalSupply = kcal.currentSupply;
+            let kcalSupplyDecimal = Math.pow(10, 10);
+            kcalSupply = kcalSupply / kcalSupplyDecimal;
+            kcalSupply = formatNumberWithCommas(kcalSupply);
+            console.log(soulSupply);
+            
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    onMount(async () => {
+        await getSupply();
+    });
+
+</script>
+
+<div>
+    <div>
+        <h1>{soulToken} Supply: {soulSupply}</h1>
+    </div>
+</div>
+
+<div>
+    <div>
+        <h1>{kcalToken} Supply: {kcalSupply}</h1>
+    </div>
+</div>
+
+
